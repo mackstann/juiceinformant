f9179bba448cf0331d7dd8b795c78fff8a2847c0
Nick Welch <nick@incise.org>
2014-06-05 23:16:21 -0700

    Got real hardware blink monitoring working!

--- hwmon.py
+++ hwmon.py
@@ -1,34 +1,34 @@
-import time, os, random
+import time
+
+from Adafruit_I2C import Adafruit_I2C
 
 from util import timestamp_to_base64
 
-def check_state():
+def check_state(i2c):
     # this should check the photo sensor and return True if it senses that the
     # LED is on, otherwise return False.
-    return time.time(), random.random() > 0.5
+    return time.time(), i2c.reverseByteOrder(i2c.readU16(0x8E)) > 30
 
 def run():
+    INTEGRATIONTIME_13MS      = 0x00 # 13.7ms
+    INTEGRATIONTIME_101MS     = 0x01 # 101ms
+    INTEGRATIONTIME_402MS     = 0x02 # 402ms
+    GAIN_1X                   = 0x00 # No gain
+    GAIN_16X                  = 0x10 # 16x gain
+    i2c = Adafruit_I2C(0x39)
+    i2c.write8(0x80, 0x03)     # enable the device
+    i2c.write8(0x81, GAIN_16X|INTEGRATIONTIME_13MS)
+
     f = open("blink-log", "a")
     last_state = False
     while True:
-        ts, state = check_state()
-        #if state == True and last_state == False: # mark the beginning of the blink
-        f.write(timestamp_to_base64(ts) + "\n") # deciseconds
-        f.flush()
+        ts, state = check_state(i2c)
+        if state == True and last_state == False: # mark the beginning of the blink
+            f.write(timestamp_to_base64(ts) + "\n") # deciseconds
+            f.flush()
 
-        #time.sleep(0.01)
+        time.sleep(0.01)
         last_state = state
-
-        # this is just random shit to sleep a period of time that is based on the
-        # current minute, the current memory usage, and some more random noise.
-        # once actual hardware monitoring is implemented in check_state, this will
-        # be thrown away.
-        kb = os.popen("free -k | tail -n2 | head -n1 | awk '{print $3}'").read().strip()
-        h = float(os.popen("date +%M").read().strip())/25.0
-        h += random.random()/80
-        v = ((10000000 - int(kb))/10000000.0)*(h+0.5)
-        print v
-        time.sleep(v)
 
 if __name__ == "__main__":
     try:
