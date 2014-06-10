a2a8039f9116549873936d7af6e1bb7146d3fa7a
Nick Welch <nick@incise.org>
2014-06-09 21:34:01 -0700

    Improved sensor code - use poweroff/poweron, etc.

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-import time
+#!/usr/bin/env python
 
+import datetime, sys, time
 from Adafruit_I2C import Adafruit_I2C
-
-from util import timestamp_to_base64
 
 class TSL2561:
     ADDR_FLOAT = 0x39 # Default address (pin left floating)
@@ -27,10 +26,12 @@ def poweron(i2c):
 def poweroff(i2c):
     i2c.write8(TSL2561.COMMAND_BIT|TSL2561.REGISTER_CONTROL, TSL2561.CONTROL_POWEROFF)
 
-def check_state(i2c):
-    # this should check the photo sensor and return True if it senses that the
-    # LED is on, otherwise return False.
+i2c = Adafruit_I2C(TSL2561.ADDR_FLOAT)
+poweron(i2c)
+i2c.write8(TSL2561.COMMAND_BIT|TSL2561.REGISTER_TIMING, TSL2561.GAIN_1X|TSL2561.INTEGRATIONTIME_13MS)
+poweroff(i2c)
 
+while 1:
     poweron(i2c)
     time.sleep(0.014)
     broadband = i2c.reverseByteOrder(i2c.readU16(TSL2561.COMMAND_BIT|TSL2561.WORD_BIT|TSL2561.REGISTER_CHAN0_LOW))
@@ -41,26 +42,5 @@ def check_state(i2c):
     isolated_ir = ir - background*2
     blink = isolated_ir >= 10
 
-    return time.time(), blink
-
-def run():
-    i2c = Adafruit_I2C(TSL2561.ADDR_FLOAT)
-    poweron(i2c)
-    i2c.write8(TSL2561.COMMAND_BIT|TSL2561.REGISTER_TIMING, TSL2561.GAIN_1X|TSL2561.INTEGRATIONTIME_13MS)
-    poweroff(i2c)
-
-    f = open("blink-log", "a")
-    last_state = False
-    while True:
-        ts, state = check_state(i2c)
-        if state == True and last_state == False: # mark the beginning of the blink
-            f.write(timestamp_to_base64(ts) + "\n") # deciseconds
-            f.flush()
-
-        last_state = state
-
-if __name__ == "__main__":
-    try:
-        run()
-    except KeyboardInterrupt:
-        pass
+    print '{},{},{},{},{},{}'.format(datetime.datetime.now().isoformat(), broadband, ir, background, isolated_ir, 10 if blink else 0)
+    sys.stdout.flush()
